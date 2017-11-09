f932e51a0e3e079a9406b77be6fedd8c39d33a99
Weichen <weichenruge@163.com>
2017-11-09 14:03:49 +0800

    learn test

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+import time
+n = int(input('输入时间：'))
+count = 0
+while (count < n):
+    time.sleep(1)
+    print('the count is:',count +1)
+    count += 1
+print('Good bye!')
+    
